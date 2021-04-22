698ea7e6c3d5bc75df7f9cec16939386926a1a6e
AlexPriahin <78930065+AlexPriahin@users.noreply.github.com>
2021-04-22 22:11:26 +0300

    Add files via upload

---
+++ AbstractStack.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Wrap.h"
+
+template <typename T>
+class AbstractStack
+{
+public:
+	virtual void Push(T item) = 0;
+	virtual Wrap<T>* Pop(int _pos) = 0;
+	virtual Wrap<T>* Get(int _pos) = 0;
+	virtual int getSize() = 0;
+};
+
